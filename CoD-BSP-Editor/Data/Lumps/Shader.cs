7f1bf2c1acfa50a701c227630fc740b80204485a
Jakub Rutkowski <jakubrutkowski3012@gmail.com>
2022-02-15 19:28:54 +0100

    Changed how Shader.Material is serialized and deserialized

--- CoD-BSP-Editor/Data/Lumps/Shader.cs
+++ CoD-BSP-Editor/Data/Lumps/Shader.cs
@@ -1,27 +1,24 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Runtime.InteropServices;
-using System.Text.Json.Serialization;
 
 namespace CoD_BSP_Editor.Data
 {
-    [StructLayout(LayoutKind.Sequential, Pack = 1)]
+    [StructLayout(LayoutKind.Sequential, Pack = 1, CharSet = CharSet.Ansi)]
     public struct Shader
     {
         [MarshalAs(UnmanagedType.ByValArray, SizeConst = 64)]
+        [JsonConverter(typeof(ShaderMaterialConverter))]
         public char[] Material;
 
         public uint SurfaceFlag;
         public uint ContentFlag;
 
-        public override string ToString()
-        {
-            return new string(this.Material).Trim('\0').ToLower();
-        }
-
+        [JsonConstructor]
         public Shader(string material, uint surfaceFlag = 0, uint contentFlag = 0)
         {
             material = material.ToLower();
@@ -30,6 +27,37 @@ namespace CoD_BSP_Editor.Data
             Material = matBuilder.ToString().ToCharArray();
             SurfaceFlag = surfaceFlag;
             ContentFlag = contentFlag;
+        }
+
+        public override string ToString()
+        {
+            return new string(this.Material).Trim('\0').ToLower();
+        }
+    }
+
+    public class ShaderMaterialConverter : JsonConverter
+    {
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            char[] material = (char[]) value;
+            string materialString = new string(material).Trim('\0').ToLower();
+
+            writer.WriteValue(materialString);
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            throw new NotImplementedException("Unnecessary because CanRead is false. The type will skip the converter.");
+        }
+
+        public override bool CanRead
+        {
+            get { return false; }
+        }
+
+        public override bool CanConvert(Type objectType)
+        {
+            return objectType == typeof(char[]);
         }
     }
 }
